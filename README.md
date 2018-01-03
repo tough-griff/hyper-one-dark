e0fe184644849c1a03995657a7a5e892751b4139
Griffin Yourick <gryphon92@gmail.com>
2018-01-03 14:40:41 -0500

    Initial updates

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
 # Atom One Dark for Hyper
 
-[Hyper](https://hyper.is) theme based on [the Atom One Dark theme](https://github.com/atom/one-dark-syntax). Forked from @jxnblk's [HyperTerminator](https://github.com/jxnblk/hyperterminator) and modified by @mdo.
-
-![Screenshot at start](https://cloud.githubusercontent.com/assets/98681/16899205/f644411e-4baf-11e6-851a-930070779867.png)
-![Screenshot with tabs](https://cloud.githubusercontent.com/assets/98681/16899206/f644c080-4baf-11e6-890d-fd5c628c7991.png)
-
-### Install
-
-1. Open Hyper's preferences with `Cmd+,` (or manually at `~/.hyper.js`) with your editor.
-2. Update your list of plugins to include `hyperterm-atom-dark`, like so:
-  
-  ```js
-plugins: [
-  'hyperterm-atom-dark'
-],
-```
-3. Fully reload Hyper (`Cmd+Shift+R`), and tada!
+[Hyper](https://hyper.is) theme based on [the Atom One Dark theme](https://github.com/atom/one-dark-syntax). Forked from [mdo/hyperterm-atom-dark](https://github.com/mdo/hyperterm-atom-dark), which is in turn forked from 
+[jxnblk/hyperterminator](https://github.com/jxnblk/hyperterminator)
 
 ### License
-
 MIT
--- index.js
+++ index.js
@@ -1,28 +1,45 @@
+const darkBlack = '#181a1f';
+const black = '#21252b';
+const darkGrey = '#282c34';
+const mediumGrey = '#5c6370';
+const lightGrey = '#abb2bf';
+const white = '#fff';
 
-const backgroundColor = '#282c34'
-const foregroundColor = '#abb2bf'
-const cursorColor = foregroundColor
-const borderColor = backgroundColor
+const red = '#e06c75';
+const yellow = '#d19a66';
+const green = '#98c379';
+const cyan = '#56b6c2';
+const blue = '#61afef';
+const magenta = '#c678dd';
+
+const borderBlue = '#528bff';
+
+const backgroundColor = darkGrey;
+const foregroundColor = lightGrey;
+const transparentForegroundColor = 'rgba(171, 178, 191, 0.4)';
+const cursorColor = foregroundColor;
+const borderColor = backgroundColor;
 
 const colors = {
-  black       : backgroundColor,
-  red         : '#e06c75', // red
-  green       : '#98c379', // green
-  yellow      : '#d19a66', // yellow
-  blue        : '#56b6c2', // blue
-  magenta     : '#c678dd', // pink
-  cyan        : '#56b6c2', // cyan
-  white       : '#d0d0d0', // light gray
-  lightBlack  : '#808080', // medium gray
-  lightRed    : '#e06c75', // red
-  lightGreen  : '#98c379', // green
-  lightYellow : '#d19a66', // yellow
-  lightBlue   : '#56b6c2', // blue
-  lightMagenta: '#c678dd', // pink
-  lightCyan   : '#56b6c2', // cyan
-  colorCubes  : '#ffffff', // white
-  grayscale   : foregroundColor
-}
+  black,
+  red,
+  green,
+  yellow,
+  blue,
+  magenta,
+  cyan,
+  white: lightGrey,
+  lightBlack: mediumGrey,
+  lightRed: red,
+  lightGreen: green,
+  lightYellow: yellow,
+  lightBlue: blue,
+  lightMagenta: magenta,
+  lightCyan: cyan,
+  lightWhite: white,
+  colorCubes: white,
+  grayscale: lightGrey,
+};
 
 exports.decorateConfig = config => {
   return Object.assign({}, config, {
@@ -33,86 +50,82 @@ exports.decorateConfig = config => {
     colors,
     termCSS: `
       ${config.termCSS || ''}
-      .cursor-node {
-        mix-blend-mode: difference;
-        border-left-width: 2px;
-      }
+      
       ::-webkit-scrollbar {
         width: 4px;
       }
       ::-webkit-scrollbar-thumb {
+        background-color: ${mediumGrey};
         border-radius: 5px;
-        background-color: #3c424e;
+      }
+      
+      .cursor-node {
+        border-left-width: 2px;
+        mix-blend-mode: difference;
       }
     `,
     css: `
       ${config.css || ''}
+      
       .header_header {
-        top: 0;
-        right: 0;
         left: 0;
+        right: 0;
+        top: 0;
       }
-      .tabs_list {
-        background-color: #21252b !important;
-        border-bottom-color: #181a1f !important;
-      }
+      
       .splitpane_divider {
-        background-color: rgba(171, 178, 191, 0.15) !important;
+        background-color: rgba(171, 178, 191, 0.15) !important; 
       }
+      
+      .tabs_list {
+        background-color: ${black};
+      }
+      
       .tab_tab {
-        font-weight: 500;
-        color: rgba(157, 165, 180, 0.6);
-        border-width: 0 0 0 1px;
-        border-image: linear-gradient(#21252b, #181a1f 1em) 0 0 0 1 stretch;
         border-style: solid;
+        border-width: 0 0 0 1px;
+        color: ${transparentForegroundColor};
+        font-weight: 500;
       }
       .tab_tab:first-of-type {
         border-width: 0;
       }
       .tab_tab:hover {
-        color: rgba(157, 165, 180, 0.6);
+        color: ${foregroundColor};
         transition: none;
       }
       .tab_tab::after {
-        content: "";
-        position: absolute;
-        pointer-events: none;
-        top: 0;
+        background: ${borderBlue};
+        border-bottom: 1px solid ${backgroundColor};
         bottom: -1px;
-        left: 0;
-        width: 2px;
+        content: "";
         height: inherit;
-        background: #528bff;
+        left: 0;
         opacity: 0;
+        pointer-events: none;
+        position: absolute;
+        top: 0;
         transition: opacity .16s;
+        width: 2px;
         z-index: 1;
       }
       .tabs_title,
       .tab_tab.tab_active {
+        color: ${foregroundColor};
         font-weight: 500;
-        color: #d7dae0;
       }
       .tab_tab.tab_active {
         background-color: ${backgroundColor};
       }
-      .tab_tab.tab_active,
-      .tab_tab.tab_active + .tab_tab {
-        border-image: linear-gradient(transparent, transparent) 0 0 0 1 stretch;
-      }
       .tab_tab.tab_active::before {
-        content: "";
-        z-index: 1;
-        position: absolute;
-        top: 0;
-        left: -1px;
         bottom: -1px;
-        right: 0;
+        content: "";
         height: inherit;
-        background-image: linear-gradient(rgba(255, 255, 255, 0.02), rgba(255, 255, 255, 0));
-        box-shadow: inset 0 1px 1px rgba(255, 255, 255, 0.06);
-        border: 1px solid #181a1f;
-        border-bottom-color: ${backgroundColor};
-        border-top: 0;
+        left: -1px;
+        position: absolute;
+        right: 0;
+        top: 0;
+        z-index: 1;
       }
       .tab_tab.tab_active:last-of-type::before {
         border-right-width: 0;
@@ -121,49 +134,44 @@ exports.decorateConfig = config => {
         opacity: 1;
         transition-duration: .32s;
       }
+      
       .tab_icon {
+        background: ${transparentForegroundColor};
+        border-radius: 0;
         display: block;
-        background: rgba(157, 165, 180, 0.6);
+        height: 100%;
         -webkit-mask-image: url('${__dirname}/close.svg');
         mask-image: url('${__dirname}/close.svg');
-        -webkit-mask-size: 7px;
-        mask-size: 7px;
-        -webkit-mask-repeat: no-repeat;
-        mask-repeat: no-repeat;
-        -webkit-mask-position-y: center;
-        mask-position-y: center;
         -webkit-mask-position-x: 8px;
         mask-position-x: 8px;
-        width: 26px;
-        height: 100%;
-        top: 0;
+        -webkit-mask-position-y: center;
+        mask-position-y: center;
+        -webkit-mask-repeat: no-repeat;
+        mask-repeat: no-repeat;
+        -webkit-mask-size: 7px;
+        mask-size: 7px;
         right: 0;
+        top: 0;
         transform: scale(0);
         transition: transform .08s;
-        opacity: 1;
-        border-radius: 0;
+        width: 26px;
         z-index: 2;
       }
-      .tab_icon:hover {
-        background: rgba(157, 165, 180, 0.6);
-        opacity: .7;
-      }
       .tab_tab.tab_active .tab_icon {
-        background: #d7dae0;
+        background: ${transparentForegroundColor};
       }
+      .tab_icon:hover,
       .tab_tab.tab_active .tab_icon:hover {
-        background: #d7dae0;
+        background: ${foregroundColor};
       }
       .tab_tab:hover .tab_icon {
         transform: scale(1);
         transition-duration: .16s;
       }
-      .tab_icon svg {
-        display: none;
-      }
+      .tab_icon svg,
       .tab_icon::after {
         display: none;
       }
-    `
-  })
-}
+    `,
+  });
+};
